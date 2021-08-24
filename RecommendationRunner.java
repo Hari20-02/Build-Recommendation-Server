16e88de877d2c6d0e6855ce0aa3919a5944e5472
Hari20-02 <86453757+Hari20-02@users.noreply.github.com>
2021-08-24 10:23:48 -0400

    Add files via upload

---
+++ RecommendationRunner.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class RecommendationRunner implements Recommender {
+    
+    public ArrayList<String> getItemsToRate () {//to rate an array list of movies
+        ArrayList<String> itemsToRate = new ArrayList<String> ();
+        ArrayList<String> movies = MovieDatabase.filterBy(new TrueFilter());
+        for (int i=0; i < 20; i++) {
+            Random rand = new Random();
+            int random = rand.nextInt(movies.size());
+            if (! itemsToRate.contains(movies.get(random))) {
+                itemsToRate.add(movies.get(random));}}
+        return itemsToRate;}
+    
+    public void printRecommendationsFor (String webRaterID) {
+        FourthRatings fourthRatings = new FourthRatings ();
+        MovieDatabase.initialize("ratedmoviesfull");
+        RaterDatabase.initialize("ratings");
+        System.out.println("List Data for " + Integer.toString(MovieDatabase.size()) + " movies");
+        System.out.println("List Data for " + Integer.toString(RaterDatabase.size()) + " raters");
+        int numSimilarRaters = 50;
+        int minNumOfRatings = 5;
+        ArrayList<Rating> similarRatings = fourthRatings.getSimilarRatings(webRaterID, numSimilarRaters, minNumOfRatings);
+        if (similarRatings.size() == 0) {
+            System.out.println("No matching movies were found");}
+        else {
+            String header = ("<table> <tr> <th>Movie Title</th> <th>Rating Value</th>  <th>Genres</th> </tr>");
+            String body = "";
+            for (Rating rating : similarRatings) {
+                body += "<tr> <td>" + MovieDatabase.getTitle(rating.getItem()) + "</td> <td>" 
+                + Double.toString(rating.getValue()) + "</td> <td>" + MovieDatabase.getGenres(rating.getItem())
+                + "</td> </tr> ";}
+            System.out.println(header  + body + "</table>");}}}
